b98108cc21a459aaa80327f761d79c6efaebca14
mcbottcher <mcbottcher@gmail.com>
2020-05-11 14:55:47 +0100

    added ros package

---
+++ examples/ros_example/catkin_ws/build/line_follower/CMakeFiles/robot.exe.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/robot.exe.dir/run.cpp.o"
+  "CMakeFiles/robot.exe.dir/Racer.cpp.o"
+  "CMakeFiles/robot.exe.dir/bandpass.cpp.o"
+  "/home/mcbottcher/enkiSimulator/examples/ros_enki/catkin_ws/devel/lib/enki_package/robot.exe.pdb"
+  "/home/mcbottcher/enkiSimulator/examples/ros_enki/catkin_ws/devel/lib/enki_package/robot.exe"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/robot.exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
